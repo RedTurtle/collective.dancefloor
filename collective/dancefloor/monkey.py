eda0a2643bd26749300f9efcc14d20fa917f6851
micecchi <micecchi@db7f04ef-aaf3-0310-a811-c281ed44c4ad>
2012-07-05 10:29:29 +0000

    fixed subscription portlet

--- collective/dancefloor/monkey.py
+++ collective/dancefloor/monkey.py
@@ -4,6 +4,9 @@ from Products.CMFPlone.interfaces import IPloneSiteRoot
 from collective.dancing.composer import template_var
 from collective.dancefloor.interfaces import IDanceFloorParty
 from collective.dancing import MessageFactory as _
+from zope.component import getMultiAdapter
+from Acquisition import aq_inner
+
 
 def composer_more_vars(self, subscription, items):
         """Less generic variables.
@@ -31,13 +34,21 @@ def composer_more_vars(self, subscription, items):
         vars['to_addr'] = '$%s' % template_var('to_addr')
         return vars
 
+
 def status_already_subscribed(self):
-    channel_url=""
+    channel_url = ""
     for item in self.context.aq_chain:
-        if IDanceFloorParty.providedBy(item):
-            channel_url="%s/portal_newsletters" %item.absolute_url()
+        if not channel_url:
+            if IDanceFloorParty.providedBy(item):
+                channel_url = "%s/portal_newsletters" % item.absolute_url()
+            else:
+                plone_view = getMultiAdapter((aq_inner(item), self.request), name='plone')
+                if plone_view.isDefaultPageInFolder():
+                    right_context = plone_view.getParentObject()
+                    if IDanceFloorParty.providedBy(right_context):
+                        channel_url = "%s/portal_newsletters" % right_context.absolute_url()
     if not channel_url:
-        channel_url=self.newslettertool.absolute_url()
+        channel_url = self.newslettertool.absolute_url()
     link_start = '<a href="%s/sendsecret.html">' % (channel_url)
     link_end = '</a>'
     # The link_start plus link_end construction is not very
